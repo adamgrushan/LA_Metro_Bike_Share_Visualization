60062df409e77716f1f96e1c57ceaf0d7991df4d
Adam Grushan <adamjgrushan@gmail.com>
2020-05-06 15:46:29 -0700

    messing around in jupyter

--- Bike Data Geographic Plot.py
+++ Bike Data Geographic Plot.py
@@ -1,7 +1,7 @@
 
 """"
 This file reads in bike share 2020 Q1 data and plots all the starting and
-ending lats and longs of a specific day on a map using matplotlib.
+ending lats and lons of the 2020 Q1 data on a map using matplotlib.
 """
 
 # Imports
@@ -33,12 +33,24 @@ if __name__ == '__main__':
 
     df_lat_lon = df_all[['start_lat', 'start_lon', 'end_lat', 'end_lon']]
 
+    data_type_list = df_all.dtypes
+    print(data_type_list)
+
+    df_starts = df_lat_lon.iloc[:, 0:2]
+    df_ends = df_lat_lon.iloc[:, 2:4]
+
+    new_columns = ["lat", "lon"]
+    df_starts.columns = new_columns
+    df_ends.columns = new_columns
+
+    df_stacked = pd.concat([df_starts, df_ends], ignore_index=True)
+
     # determine min and maxes of start/end lat and start/end lon
 
-    min_lon = min(df_lat_lon.start_lon.min(), df_lat_lon.end_lon.min())
-    max_lon = max(df_lat_lon.start_lon.max(), df_lat_lon.end_lon.max())
-    min_lat = min(df_lat_lon.start_lat.min(), df_lat_lon.end_lat.min())
-    max_lat = max(df_lat_lon.start_lat.max(), df_lat_lon.end_lat.max())
+    min_lon = df_stacked.lon.min()
+    max_lon = df_stacked.lon.max()
+    min_lat = df_stacked.lat.min()
+    max_lat = df_stacked.lat.max()
 
     BBox = (min_lon, max_lon,
             min_lat, max_lat)
@@ -49,15 +61,13 @@ if __name__ == '__main__':
 
     la_map = plt.imread('Data/LA_Map.png')
 
-    # plot points
+    # plot all lats and lons
 
     fig, ax = plt.subplots(figsize=(8, 7))
-    ax.scatter(df_lat_lon.start_lon, df_lat_lon.start_lat, zorder=1, alpha=0.005, c='b', s=10)
+    ax.scatter(df_stacked.lon, df_stacked.lat, zorder=1, alpha=0.002, c='b', s=10)
     ax.set_title('Plotting Bike Locations on Los Angeles Map')
     ax.set_xlim(BBox[0], BBox[1])
     ax.set_ylim(BBox[2], BBox[3])
     ax.imshow(la_map, zorder=0, extent=BBox, aspect='equal')
 
     plt.show()
-
-
